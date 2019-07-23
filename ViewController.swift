9b992687b45aad813585a497073e83707a7a0b4d
Allen Su <37977270+allensu02@users.noreply.github.com>
2019-07-23 14:29:36 -0700

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     var problemOperation = 0
     
     let operations = ["+", "-", "*", "/"]
+    let buttonColor = UIColor(hue: 0.325, saturation: 0.14, brightness: 0.97, alpha: 1.0)
     
     @IBOutlet weak var numberOneLabel: UILabel!
     @IBOutlet weak var numberTwoLabel: UILabel!
@@ -39,62 +40,45 @@ class ViewController: UIViewController {
         newGame()
     }
     
-    
-    @IBAction func startOverButton(_ sender: Any) {
-        newGame()
-    }
-    func newGame () {
-        problemTotal = 0
-        problemCorrect = 0
-        problemNumber = 1
-        generateNewProblem()
-        updateLabels()
-    }
-
     func generateNewProblem () {
         problemOperation = Int.random(in: 0...3)
-
+        
         if problemOperation == 0 {
             //addition problem
             numberOne = Int.random(in: 5...50)
             numberTwo = Int.random(in: 5...50)
             correctAnswer = numberOne + numberTwo
-            options[0] = correctAnswer - 1
-            options[1] = correctAnswer + 1
-            options[2] = correctAnswer - 10
-            options[3] = correctAnswer
-            
         } else if problemOperation == 1 {
             //subtraction problem
             numberOne = Int.random(in: 50...100)
             numberTwo = Int.random(in: 5...40)
             correctAnswer = numberOne - numberTwo
-            options[0] = correctAnswer - 1
-            options[1] = correctAnswer + 1
-            options[2] = correctAnswer - 10
-            options[3] = correctAnswer
         } else if problemOperation == 2 {
             //multiplication problem
             numberOne = Int.random(in: 2...10)
             numberTwo = Int.random(in: 2...10)
             correctAnswer = numberOne * numberTwo
-            options[0] = correctAnswer - 1
-            options[1] = correctAnswer + 1
-            options[2] = correctAnswer - 10
-            options[3] = correctAnswer
         } else if problemOperation == 3 {
             //division problem
             numberTwo = Int.random(in: 3...10)
             numberOne = Int.random(in: 3...10) * numberTwo
             correctAnswer = numberOne/numberTwo
-            options[0] = correctAnswer - 1
-            options[1] = correctAnswer + 1
-            options[2] = correctAnswer - 2
-            options[3] = correctAnswer
         }
+        generatingAnswerOptions()
+    }
+    
+    func generatingAnswerOptions() {
+        var i = 0
+        while i < 3 {
+            var n = 0
+            while n == 0 {
+                n = Int.random(in: -10...10)
+                options[i] = correctAnswer + n
+            }
+            i += 1
+        }
+        options[3] = correctAnswer
         options.shuffle()
-        
-        
     }
     
     
@@ -102,56 +86,33 @@ class ViewController: UIViewController {
         numberOneLabel.text = String(numberOne)
         numberTwoLabel.text = String(numberTwo)
         operationLabel.text = operations[problemOperation]
-        optionOneButton.setTitle(String(options[0]), for: .normal)
-        optionTwoButton.setTitle(String(options[1]), for: .normal)
-        optionThreeButton.setTitle(String(options[2]), for: .normal)
-        optionFourButton.setTitle(String(options[3]), for: .normal)
+        
+        updateOptions(option: optionOneButton, num: 1)
+        updateOptions(option: optionTwoButton, num: 2)
+        updateOptions(option: optionThreeButton, num: 3)
+        updateOptions(option: optionFourButton, num: 4)
+        
         problemCorrectLabel.text = String(problemCorrect)
         problemTotalLabel.text = String(problemTotal)
         currentProblemLabel.text = String(problemNumber)
-    }
-    
-    @IBAction func optionOneTouched(_ sender: Any) {
-        if optionOneButton.currentTitle == String(correctAnswer) {
-            answerCorrect()
-        } else {
-            answerIncorrect()
-        }
-    }
-    
-    @IBAction func optionTwoTouched(_ sender: Any) {
-        if optionTwoButton.currentTitle == String(correctAnswer) {
-            answerCorrect()
-        } else {
-            answerIncorrect()
-        }
-    }
-    
-    @IBAction func optionThreeTouched(_ sender: Any) {
-        if optionThreeButton.currentTitle == String(correctAnswer) {
-            answerCorrect()
-        } else {
-            answerIncorrect()
-        }
-    }
-    
-    @IBAction func optionFourTouched(_ sender: Any) {
-        if optionFourButton.currentTitle == String(correctAnswer) {
-            answerCorrect()
-        } else {
-            answerIncorrect()
-        }
-    }
-    
-    func answerCorrect () {
-        problemCorrect += 1
-        problemTotal += 1
-        nextProblem()
         
+        enableButtons()
     }
     
-    func answerIncorrect () {
-        problemTotal += 1
+    func updateOptions (option: UIButton, num: Int) {
+        option.setTitle(String(options[num-1]), for: .normal)
+        option.setBackgroundColor(color: buttonColor, forState: .normal)
+    }
+    
+    func newGame () {
+        problemTotal = 0
+        problemCorrect = 0
+        problemNumber = 1
+        generateNewProblem()
+        updateLabels()
+    }
+    
+    @IBAction func nextButton(_ sender: Any) {
         nextProblem()
     }
     
@@ -160,5 +121,62 @@ class ViewController: UIViewController {
         generateNewProblem()
         updateLabels()
     }
+    
+    @IBAction func startOverButton(_ sender: Any) {
+        newGame()
+    }
+
+    @IBAction func optionOneTouched(_ sender: Any) {
+        buttonTouched(option: optionOneButton)
+    }
+    
+    @IBAction func optionTwoTouched(_ sender: Any) {
+        buttonTouched(option: optionTwoButton)
+    }
+    
+    @IBAction func optionThreeTouched(_ sender: Any) {
+        buttonTouched(option: optionThreeButton)
+    }
+    
+    @IBAction func optionFourTouched(_ sender: Any) {
+        buttonTouched(option: optionFourButton)
+    }
+    
+    //checks if the option is the correct answer and responds accordingly
+    func buttonTouched (option: UIButton) {
+        if option.currentTitle == String(correctAnswer) {
+            answerCorrect(option: option)
+        } else {
+            answerIncorrect(option: option)
+        }
+    }
+    
+    func answerCorrect (option: UIButton) {
+        problemCorrect += 1
+        problemTotal += 1
+        option.setBackgroundColor(color: UIColor.green, forState: .normal)
+        disableButtons()
+    }
+    
+    func answerIncorrect (option: UIButton) {
+        problemTotal += 1
+        option.setBackgroundColor(color: UIColor.red, forState: .normal)
+        disableButtons()
+    }
+    
+    func disableButtons () {
+        optionOneButton.isEnabled = false
+        optionTwoButton.isEnabled = false
+        optionThreeButton.isEnabled = false
+        optionFourButton.isEnabled = false
+    }
+    
+    func enableButtons () {
+        optionOneButton.isEnabled = true
+        optionTwoButton.isEnabled = true
+        optionThreeButton.isEnabled = true
+        optionFourButton.isEnabled = true
+    }
+    
 }
 
